3694030b88e3b538df2881e67bbfbebe792d1cf7
keoyomini <155010132+mini2dou@users.noreply.github.com>
2026-01-26 01:03:49 +0900

    s

--- app/worldcup/page.tsx
+++ app/worldcup/page.tsx
@@ -1,5 +1,6 @@
-"use client";
 export const dynamic = "force-dynamic";
+
+"use client";
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
